0f6139ceece76724d5d07cef5fd0180e5b4691ce
k4zuy <s84438@htw-dresden.de>
2024-02-05 08:03:48 +0100

    added 'Mensa Siedepunkt' with parameter '-siedepunkt'; added ASCII art for 'Mensa Siedepunkt', added category to distinguish between lunch and dinner meals; added todays date as print statement; added '€' sign after meal price

--- mensaToday.py
+++ mensaToday.py
@@ -15,17 +15,22 @@ def PrettyPrintJson(data) -> None:
                 for nested_key, nested_value in value.items():
                     output += f"  {nested_key}: {nested_value}\n"
             else:
-                output += f"\033[1m{key}:\033[0m \033[33m{value}\033[0m\n"
+                if key == "price":
+                    output += f"\033[1m{key}:\033[0m \033[33m{value}€\033[0m\n"
+                else:
+                    output += f"\033[1m{key}:\033[0m \033[33m{value}\033[0m\n"
     print(output)
 
 lastparam = sys.argv[-1]
 
-today = datetime.datetime.today().strftime("%Y-%m-%d")
+today = datetime.datetime.today()
+todaystr = today.strftime("%Y-%m-%d")
 matrixlat = 51.034128
 matrixlon = 13.733597
 mensaIds = {
     '-matrix': 6,
-    '-zelt': 35
+    '-zelt': 35,
+    '-siedepunkt': 9
 }
 asciiArts = { mensaIds['-matrix'] : ''' 
 \033[36m----------------------------------------------------------------------------
@@ -42,15 +47,25 @@ asciiArts = { mensaIds['-matrix'] : '''
 |   /    __/  |  |         | (     | | |  |  (   |   __/ \__ \ \__ \  (     | | |   __/  |   | |
 | ____|\___| _| \__| _____/ \___| _| |_| _| \___/  \___| ____/ ____/ \___| _| |_| \___| _|  _| |
 ------------------------------------------------------------------------------------------------\033[0m          
+''',
+            mensaIds['-siedepunkt'] : ''' 
+\033[36m------------------------------------------------------------------------------------------------
+ __  __                              _____  _            _                             _     _   
+|  \/  |  ___  _ __   ___   __ _    / ____|(_)  ___   __| |  ___  _ __   _   _  _ __  | | __| |_ 
+| |\/| | / _ \| '_ \ / __| / _` |   | (___  _  / _ \ / _` | / _ \| '_ \ | | | || '_ \ | |/ /| __|
+| |  | ||  __/| | | |\__ \| (_| |    \___ \| ||  __/| (_| ||  __/| |_) || |_| || | | ||   < | |_ 
+|_|  |_| \___||_| |_||___/ \__,_|    ____/ |_| \___| \__,_| \___|| .__/  \__,_||_| |_||_|\_\ \__|
+------------------------------------------------------------------------------------------------\033[0m          
 '''
 }
 id = mensaIds[lastparam] if lastparam in mensaIds else mensaIds['-matrix']
 url="https://api.studentenwerk-dresden.de/openmensa/v2"
 #endpoint = f"{url}/canteens?near[lat]={matrixlat}&near[lng]={matrixlon}"
-endpoint = f"{url}/canteens/{id}/days/{today}/meals"
+endpoint = f"{url}/canteens/{id}/days/{todaystr}/meals"
 meals = requests.request("GET", endpoint).json()
-dataToPrint = [{"name": re.sub(r" \(.*?\)", "", meal["name"]), "price": meal["prices"]["Studierende"]} for meal in meals]
+dataToPrint = [{"category": meal["category"],"name": re.sub(r" \(.*?\)", "", meal["name"]), "price": meal["prices"]["Studierende"]} for meal in meals]
 #Maybe fix? ->
 #dataToPrint = [{"name": re.sub(r" \(.*?\)", "", meal["name"]), "price": meal["prices"]["Studierende"]} for meal in meals]
 print(asciiArts[id])
+print(f"\033[92m{today.strftime('%d.%m.%Y')}\033[0m\n")
 PrettyPrintJson(dataToPrint)
